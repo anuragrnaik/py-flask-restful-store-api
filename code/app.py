9ee4a7d13cc9ad75320035b1ae333566996e9c97
Anurag R Naik <anaik@webmd.net>
2021-11-30 20:05:19 +0530

    Fix db read issue

--- code/app.py
+++ code/app.py
@@ -1,9 +1,9 @@
+import os
 from datetime import timedelta
 from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt import JWT
 
-from db import db, get_connection
 from security import authenticate, identity
 
 from resources.item import Item, Items
@@ -11,7 +11,12 @@ from resources.store import Store, StoreList
 from resources.user import UserRegister
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = get_connection()
+
+db_conn = os.getenv('DATABASE_URL', 'sqlite:///data.db')  # or other relevant config var
+if db_conn.startswith("postgres://"):
+    uri = db_conn.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = db_conn
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'john doe'
 app.config['JWT_AUTH_URL_RULE'] = '/login'
@@ -21,10 +26,6 @@ app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=1800)
 # app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
 
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 
@@ -49,5 +50,10 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
+
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
