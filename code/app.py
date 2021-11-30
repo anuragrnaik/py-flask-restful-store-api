58d018cbf048ae1d6f878e36a7b783e3c7c6c1ac
Anurag R Naik <anaik@webmd.net>
2021-11-30 19:20:11 +0530

    Added support to postgresql for heroku

--- code/app.py
+++ code/app.py
@@ -10,8 +10,6 @@ from resources.store import Store, StoreList
 from resources.user import UserRegister
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'john doe'
 app.config['JWT_AUTH_URL_RULE'] = '/login'
 # config JWT to expire within half an hour
@@ -44,6 +42,8 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    from db import db
+    from db import db, get_connection
+    app.config['SQLALCHEMY_DATABASE_URI'] = get_connection()
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     app.run(port=5000, debug=True)
--- code/db.py
+++ code/db.py
@@ -1,3 +1,10 @@
+import os
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+def get_connection():
+    db_uri = os.getenv('DATABASE_URL', 'sqlite:///data.db')
+    if db_uri and db_uri.startswith("postgres://"):
+        db_uri = db_uri.replace("postgres://", "postgresql://", 1)
+    return db_uri
